909d99cee8e837abf0080a9718e66e4b39a724c2
Alex Shvl <alex@shvl.tech>
2024-04-25 21:29:58 +0200

    fixed script

--- readToBuffer.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const readToBuffer = (path) => {
-    const buffer = fs.readFileSync(path);
-    const float32Array = new Float32Array(buffer);
-    return JSON.stringify([...float32Array]);
-}
-
-console.log(readToBuffer('./weights/face_landmark_68_model-shard1'))
